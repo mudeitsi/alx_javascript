4f287a1ead0368fc1a359c62f7a54a5d7e076457
mudeitsi <eserenash@gmail.comm>
2023-09-12 02:27:00 -0700

    task 0 and README.md file

---
+++ Javascript_advanced/0-welcome.js
@@ -0,0 +1,21 @@
+function welcome(firstName, lastName) {
+  /**
+   * This function takes two arguments, firstName and lastName, and provides a welcome message.
+   * It also calls another inner function, displayFullName, to display the welcome message in an alert.
+   */
+  
+  // Concatenating the firstName and lastName to form the fullName
+  const fullName = `${firstName} ${lastName}`;
+
+  function displayFullName() {
+    /**
+     * This inner function displays an alert with a welcome message using the fullName.
+     */
+    alert(`Welcome ${fullName}!`);
+  }
+
+  // Call the inner function to display the alert
+  displayFullName();
+}
+
+
